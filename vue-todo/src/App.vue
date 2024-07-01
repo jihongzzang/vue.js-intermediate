3a74111ef6040067768beca0204c53d10145fcda
jujihong <jujihong2@gmail.com>
2024-07-02 04:06:40 +0900

    feat: vue-todo-refactoring

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,19 +1,76 @@
 <template>
   <div id="app">
     <TodoHeader />
-    <TodoInput />
-    <TodoList />
-    <TodoFooter />
+    <TodoInput @addTodoItem="addOneItem" />
+    <TodoList :propsdata="todoItems" @removeItem="removeOneItem" @toggleItem="toggleOneItem" />
+    <TodoFooter @clearAll="clearAllItems" />
   </div>
 </template>
 
 <script>
-import TodoHeader from './components/TodoHeader.vue';
-import TodoInput from './components/TodoInput.vue';
-import TodoList from './components/TodoList.vue';
-import TodoFooter from './components/TodoFooter.vue';
+import TodoHeader from './components/refactored/TodoHeader.vue';
+import TodoInput from './components/refactored/TodoInput.vue';
+import TodoList from './components/refactored/TodoList.vue';
+import TodoFooter from './components/refactored/TodoFooter.vue';
 
 export default {
+  data() {
+    return {
+      todoItems: [],
+    };
+  },
+
+  methods: {
+    addOneItem(todoItem) {
+      const exists = this.todoItems.some((item) => item.item === todoItem);
+
+      if (exists) {
+        alert('이미 추가하신 항목입니다.');
+        return;
+      }
+
+      var obj = { completed: false, item: todoItem };
+      this.todoItems.push(obj);
+      this.saveTodoItems();
+    },
+
+    removeOneItem(index) {
+      this.todoItems.splice(index, 1);
+      this.saveTodoItems();
+    },
+
+    toggleOneItem(index) {
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+      this.saveTodoItems();
+    },
+
+    clearAllItems() {
+      this.todoItems = [];
+      localStorage.clear();
+    },
+
+    saveTodoItems() {
+      localStorage.setItem('todoItems', JSON.stringify(this.todoItems));
+    },
+
+    loadTodoItems() {
+      const savedItems = localStorage.getItem('todoItems');
+
+      if (savedItems) {
+        try {
+          this.todoItems = JSON.parse(savedItems);
+        } catch (e) {
+          console.error('Error loading items from localStorage', e);
+          this.todoItems = [];
+        }
+      }
+    },
+  },
+
+  created() {
+    this.loadTodoItems();
+  },
+
   components: {
     TodoHeader: TodoHeader,
     TodoInput: TodoInput,
---
+++ vue-todo/src/components/refactored/TodoList.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(todoItem, index) in propsdata" :key="todoItem.item" class="shadow">
+        <i
+          class="checkBtn fa-solid fa-check"
+          :class="{ checkBtnCompleted: todoItem.completed }"
+          @click="toggleComplete(todoItem, index)"
+        ></i>
+        <span :class="{ textCompleted: todoItem.completed }">{{ todoItem.item }}</span>
+        <span class="removeBtn" @click="removeTodo(todoItem, index)">
+          <i class="fa-solid fa-trash"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['propsdata'],
+  methods: {
+    removeTodo(_, index) {
+      this.$emit('removeItem', index);
+    },
+
+    toggleComplete(_, index) {
+      this.$emit('toggleItem', index);
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0;
+  margin-top: 0;
+  text-align: left;
+}
+
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+
+.checkBtnCompleted {
+  color: #b3adad;
+}
+
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+</style>
