c0deb3fa19a728c6c43bbc6824428dafaa0b2a0f
jujihong <jujihong2@gmail.com>
2024-07-03 00:00:43 +0900

    feat: vue-composition-life-cycle basic

--- vue-composition/src/App.vue
+++ vue-composition/src/App.vue
@@ -1,16 +1,77 @@
 <template>
-  <p>{{ message }}</p>
-  <button type="button" @click="changeMessage">change</button>
+  <TodoHeader :appTitle="title" />
+  <TodoInput @add="addTodoItem" />
+  <TodoList :todoItems="todoItems" @remove="removeTodoItem" />
 </template>
 
 <script>
-import useMessage from './hooks/useMessage';
+import { onBeforeMount, onMounted, onUnmounted, ref } from 'vue';
+
+import TodoHeader from '@/components/TodoHeader.vue';
+
+import TodoInput from '@/components/TodoInput.vue';
+
+import TodoList from '@/components/TodoList.vue';
 
 export default {
-  setup() {
-    const { message, changeMessage } = useMessage();
+  data() {
+    return {
+      title: '할일 앱',
+    };
+  },
 
-    return { message, changeMessage };
+  setup() {
+    const todoItems = ref([]);
+
+    const fetchTodos = () => {
+      const result = [];
+      for (let i = 0; i < localStorage.length; i++) {
+        const todoItem = localStorage.key(i);
+
+        result.push(todoItem);
+      }
+
+      return result;
+    };
+
+    console.log('setup called');
+
+    // 라이프 사이클 API : created()
+
+    // todoItems.value = fetchTodos();
+
+    onBeforeMount(() => {
+      console.log('onBeforeMount called');
+      todoItems.value = fetchTodos();
+    });
+
+    onMounted(() => {
+      console.log('onMounted called');
+    });
+
+    onUnmounted(() => {
+      console.log('onUnmounted called');
+    });
+
+    const addTodoItem = (todo) => {
+      todoItems.value.push(todo);
+
+      localStorage.setItem(todo, todo);
+    };
+
+    const removeTodoItem = (item, index) => {
+      todoItems.value.splice(index, 1);
+
+      localStorage.removeItem(item);
+    };
+
+    return { todoItems, addTodoItem, removeTodoItem };
+  },
+
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
   },
 };
 </script>
---
+++ vue-composition/src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in todoItems" :key="`${item}-${index}`">
+      <span>{{ item }}</span>
+      <button @click="removeTodo(item, index)">삭제</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['todoItems'],
+
+  setup(props, context) {
+    const removeTodo = (item, index) => {
+      context.emit('remove', item, index);
+    };
+
+    return { removeTodo };
+  },
+};
+</script>
