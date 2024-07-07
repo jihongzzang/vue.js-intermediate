dccd3756ba03893ea5588e528def5f541708b917
jujihong <jujihong2@gmail.com>
2024-07-08 02:37:44 +0900

    refactor: 네비게이션 가드를 통한 데이터 호출

--- vue-advanced/src/mixins/ListMixin.js
+++ vue-advanced/src/mixins/ListMixin.js
@@ -1,18 +1,23 @@
 import bus from '@/utils/bus.js';
 
 export default {
-  created() {
-    bus.$emit('start:spinner');
+  // 재사용 할 컴포넌트 옵션 & 로직
 
-    setTimeout(() => {
-      this.$store
-        .dispatch('FETCH_LIST', this.$route.name)
-        .then(() => {
-          bus.$emit('end:spinner');
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }, 500);
+  // created() {
+  //   bus.$emit('start:spinner');
+  //   setTimeout(() => {
+  //     this.$store
+  //       .dispatch('FETCH_LIST', this.$route.name)
+  //       .then(() => {
+  //         bus.$emit('end:spinner');
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   }, 500);
+  // },
+
+  mounted() {
+    bus.$emit('end:spinner');
   },
 };
--- vue-advanced/src/routes/index.js
+++ vue-advanced/src/routes/index.js
@@ -12,6 +12,10 @@ import UserView from '@/views/UserView.vue';
 
 import ItemView from '@/views/ItemView.vue';
 
+import store from '@/store';
+
+import bus from '@/utils/bus.js';
+
 Vue.use(VueRouter);
 
 const router = new VueRouter({
@@ -26,18 +30,51 @@ const router = new VueRouter({
       path: '/news',
       name: 'news',
       component: NewsView,
+      beforeEnter: (to, _, next) => {
+        bus.$emit('start:spinner');
+
+        store
+          .dispatch('FETCH_LIST', to.name)
+          .then(() => next())
+          .catch((err) => {
+            console.log(err);
+          });
+      },
+
       // component: createListView('NewsView'),
     },
     {
       path: '/jobs',
       name: 'jobs',
       component: JobsView,
+      beforeEnter: (to, _, next) => {
+        bus.$emit('start:spinner');
+
+        store
+          .dispatch('FETCH_LIST', to.name)
+          .then(() => next())
+          .catch((err) => {
+            console.log(err);
+          });
+      },
+
       // component: createListView('JobsView'),
     },
     {
       path: '/ask',
       name: 'ask',
       component: AskView,
+      beforeEnter: (to, _, next) => {
+        bus.$emit('start:spinner');
+
+        store
+          .dispatch('FETCH_LIST', to.name)
+          .then(() => next())
+          .catch((err) => {
+            console.log(err);
+          });
+      },
+
       // component: createListView('AskView'),
     },
 
