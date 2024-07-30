72395c59349b1f497f47f57d654b5b77c69e44af
jujihong <jujihong2@gmail.com>
2024-07-31 01:47:12 +0900

    controlled components

--- design/controlled/src/App.vue
+++ design/controlled/src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>
+      {{ checked }}
+    </p>
+    <check-box v-model="checked" @input="toggleCheckBox"></check-box>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CheckBox from '@/components/CheckBox.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    CheckBox,
+  },
+  data() {
+    return {
+      checked: false,
+    };
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  methods: {
+    toggleCheckBox(value) {
+      this.checked = value;
+    },
+  },
+};
+</script>
